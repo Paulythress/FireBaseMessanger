2a372b3d277b6f75e18a6ab27804e8b74295e5d6
BrandonP <BenjiPop14@gmail.com>
2019-07-24 23:50:27 -0500

    Add files via upload

---
+++ LoginActvitiy.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinmessanger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActvitiy: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+      setContentView(R.layout.activity_login)
+    }
+}
